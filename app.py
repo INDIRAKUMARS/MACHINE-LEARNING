7d8e80ecbd4dc0c557544c5ff5cfe1cd6d80a0dc
INDIRAKUMARS <indirakumar6252@gmail.com>
2026-03-14 12:53:43 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("CNN Image Classification Project")
+
+st.write("This is my Machine Learning project")
+
+number = st.number_input("Enter a number")
+
+st.write("Square:", number * number)
